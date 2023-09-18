9983bf739e85bf1572c9eb4264b84606ce19602c
Florian Weßel <florianwessel@gmx.net>
2023-09-18 21:06:53 +0200

    CMake: Disable compile commands symlink

--- lua/florian/plugins/cmake-tools.lua
+++ lua/florian/plugins/cmake-tools.lua
@@ -11,11 +11,13 @@ cmake_tools.setup({
     cmake_generate_options = { "-D", "CMAKE_EXPORT_COMPILE_COMMANDS=1" },
     cmake_build_options = {},
     cmake_console_size = 30, -- cmake output window height
-    cmake_show_console = "always", -- "always", "only_on_error"
+    cmake_show_console = "only_on_error", -- "always", "only_on_error"
     cmake_dap_configuration = { name = "cpp", type = "lldb", request = "launch" }, -- dap configuration, optional
+    cmake_soft_link_compile_commands = false,
     cmake_dap_open_command = require("dap").repl.open, -- optional
     cmake_variants_message = {
         short = { show = true },
         long = { show = true, max_length = 40 }
-    }
+    },
+    make_console_position = "bottom", -- "bottom", "top"
 })
