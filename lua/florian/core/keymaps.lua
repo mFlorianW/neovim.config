2fc1c23c690e19c2466064581d0163d6b8f4ef18
Florian Weßel <florianwessel@gmx.net>
2023-10-03 17:11:52 +0200

    Add keymap for closing buffer without closing the window

--- lua/florian/core/keymaps.lua
+++ lua/florian/core/keymaps.lua
@@ -17,6 +17,7 @@ keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+keymap.set("n", "<leader>sw", ":bp | sp | bn | bd<CR>") -- close buffer without closing the window
 
 keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
