ccddb31e270052ded591755aa68ef196b02cd2c1
Florian Weßel <florianwessel@gmx.net>
2024-01-30 20:00:39 +0100

    nvim-cmp: always display lsp results first

--- lua/florian/plugins/nvim-cmp.lua
+++ lua/florian/plugins/nvim-cmp.lua
@@ -49,10 +49,10 @@ cmp.setup({
     },
     sorting = {
         comparators = {
+            require("clangd_extensions.cmp_scores"),
+            cmp.config.compare.recently_used,
             cmp.config.compare.offset,
             cmp.config.compare.exact,
-            cmp.config.compare.recently_used,
-            require("clangd_extensions.cmp_scores"),
             cmp.config.compare.kind,
             cmp.config.compare.sort_text,
             cmp.config.compare.length,
